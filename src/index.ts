7c0294e7caea30b46d3546f505c6fbba0312a16d
にー兄さん <2237k.drumath@gmail.com>
2021-04-03 17:09:04 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './style/index.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const messageElement = document.getElementById('message');
+  messageElement!.innerHTML = '☕ TypeScript Template';
+});
