4fa65a40ad5e6915a11058cf7c75162c2c687fed
drumath <2237k.drumath@gmail.com>
2021-04-07 19:22:19 +0900

    :fire: gh-pages用変更忘れ

--- src/src/canvasManager.ts
+++ src/src/canvasManager.ts
@@ -109,8 +109,8 @@ export default class CanvasManager {
 
     SceneLoader.ImportMeshAsync(
         'trophy',
-        // '/babylon-lowpoly-showcase/scenes/',
-        '/scenes/',
+        '/babylon-lowpoly-showcase/scenes/',
+        // '/scenes/',
         'scene.babylon',
         this.scene,
         (event) => {
